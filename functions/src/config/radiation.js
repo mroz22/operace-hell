de742a46dc3bddb20065cc9581dc45c3bc46a67d
martin <mvarmuza@gmail.com>
2019-10-27 15:39:04 +0100

    a

---
+++ functions/src/config/radiation.js
@@ -0,0 +1,9 @@
+const radiation = {
+    1: 10,
+    5: 0,
+    10: 100
+}
+
+module.exports = {
+    radiation,
+}
--- functions/src/interval.js
+++ functions/src/interval.js
@@ -1,42 +1,33 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const utils = require('./utils');
+const radiation = require('./config/radiation');
+
+const getRadiationForEpoch = (epoch) => {
+    console.log(epoch);
+    const key = Object.keys(radiation).reverse().find(k => k <= epoch);
+    console.log('key', key);
+    console.log('radiation[key]', radiation[key])
+    return radiation[key];
+};
 
 exports.runInterval = functions.pubsub.topic('interval').onPublish(async () => {
         // set game state in this tick
         console.log('run interval');
     
         const game = await admin.firestore().collection('game').doc('operacexxx').get();
-        
-        const currentRadiation = game.data().radiation;
-        const currentRadiationChangeRate = game.data().radiationChangeRate;
-        const ticksToRadiationChange = game.data().ticksToRadiationChange;
-    
-        // updated GAME
+
+        // update GAME
         const timestamp = Date.now();
-        
-        // if (ticksToRadiationChange === 0) {
-        //     const nextRadiationChangeRate = utils.getRadiationRateChange(1, 0.5);
-        //     await admin.firestore().collection('game').doc('operacexxx').update({
-        //         ticksToRadiationChange: utils.getRandomInt(3, 6),
-        //         radiationChangeRate: nextRadiationChangeRate,
-        //         timestamp,
-        //     });    
-        // } else {
-         
-        let nextRadiation = currentRadiation + currentRadiationChangeRate;
-        if (nextRadiation < 0) {
-            nextRadiation = 0;
-        }
-        
+
         await admin.firestore().collection('game').doc('operacexxx').update({
-            radiation: nextRadiation,
-                // ticksToRadiationChange: ticksToRadiationChange - 1,
+            radiation: getRadiationForEpoch(game.epoch),
             timestamp,
+            epoch: game.epoch++,
         });
-        // }
+        
     
-        // updated USERS
+        // update USERS
         let users = [];
         await admin.firestore().collection('users').get().then((querySnapshot) => {
             return querySnapshot.forEach((doc) => {
@@ -46,12 +37,11 @@ exports.runInterval = functions.pubsub.topic('interval').onPublish(async () => {
                     id: doc.id,
                 })
                 const currentUserRadiation = doc.data().status.radiation;
-                const DOSE_MODIFIER = doc.data().status.protectiveSuiteOn ? 1000 : 100;
-                if (currentUserRadiation < currentRadiation) {
-                    return userRef.update({
-                        'status.radiation': currentUserRadiation + ((currentRadiation - currentUserRadiation) / DOSE_MODIFIER )
-                    });
-                }
+                // only 5% of regular radiation affects player in protectiveSuite;
+                const DOSE_MODIFIER = doc.data().status.protectiveSuiteOn ? 0.05 : 1;
+                return userRef.update({
+                    'status.radiation': currentUserRadiation + ((currentRadiation / 60 ) * DOSE_MODIFIER)
+                });
             });
         }).catch((err) => console.error(err));
     
@@ -65,19 +55,19 @@ exports.runInterval = functions.pubsub.topic('interval').onPublish(async () => {
                     ...doc.data(),
                     id: doc.id,
                 };
+                console.log(users, 'users');
                 const numberOfUsers = users.filter(u => u.BunkerId === doc.id).length;
-                
-                console.log('bunker', bunker);
-
-                if (typeof numberOfUsers !== 'number') {
+                console.log('numberOfUsers', numberOfUsers);
+                if (typeof numberOfUsers !== 'number' || isNaN(numberOfUsers)) {
                     console.error('number of users not defined in bunker, this looks like error')
                     return;
                 }
+                // nothing needs to change
                 if ((numberOfUsers === 0 && bunker.oxygenGeneration === 0) || bunker.isDestroyed) {
                     return;
                 }
     
-                let updatedOxygen = bunker.oxygen - (numberOfUsers * 0.1) + bunker.oxygenGeneration;
+                let updatedOxygen = bunker.oxygen - (numberOfUsers * 1) + bunker.oxygenGeneration;
                 if (updatedOxygen < 0) {
                     bunkerRef.update({
                         isDestroyed: true,
