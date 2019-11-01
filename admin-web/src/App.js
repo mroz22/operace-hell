561e33fbf5ddc87295628d91425f7ee1d6e047df
martin <mvarmuza@gmail.com>
2019-11-01 21:23:43 +0100

    a

--- admin-web/src/App.js
+++ admin-web/src/App.js
@@ -15,46 +15,46 @@ const A4 = styled.div`
 const SIZE=400;
 
 const App = () => {
-    const [roles, setRoles] = useState([]);
-    const [teams, setTeams] = useState([]);
-    const [game, setGame] = useState(null);
+    // const [roles, setRoles] = useState([]);
+    // const [teams, setTeams] = useState([]);
+    // const [game, setGame] = useState(null);
     const [bunkers, setBunkers] = useState([]); 
-    const [characters, setCharacters] = useState([]);
+    // const [characters, setCharacters] = useState([]);
   
     useEffect(() => {
         const db = firebase.firestore();
-        const getCharacters = async () => {
-            try {
-                const response = await fetch('./data/characters.json');
-                if (!response.ok) {
-                    // would be nice to handle error but meh..
-                    return
-                }
-                const json = await response.json();
-                setCharacters(json);
-            } catch (err) {
-                // mehh...
-            }
-        }
-        const getRoles = () => {
-            db.collection('users').onSnapshot(function(querySnapshot) {
-                const updatedRoles = [];
-                querySnapshot.forEach(function(doc) {
-                    updatedRoles.push({ id: doc.id, ...doc.data()});
-                });
-                setRoles(updatedRoles);
-            }, (error) => {
-                console.log('getRolesError', error)
-            });
-        }
-        const getGame = () => {
-            db
-            .collection("game")
-            .doc("operacexxx")
-            .onSnapshot(function(doc) {
-                setGame(doc.data());
-            });
-        }
+        // const getCharacters = async () => {
+        //     try {
+        //         const response = await fetch('./data/characters.json');
+        //         if (!response.ok) {
+        //             // would be nice to handle error but meh..
+        //             return
+        //         }
+        //         const json = await response.json();
+        //         setCharacters(json);
+        //     } catch (err) {
+        //         // mehh...
+        //     }
+        // }
+        // const getRoles = () => {
+        //     db.collection('users').onSnapshot(function(querySnapshot) {
+        //         const updatedRoles = [];
+        //         querySnapshot.forEach(function(doc) {
+        //             updatedRoles.push({ id: doc.id, ...doc.data()});
+        //         });
+        //         setRoles(updatedRoles);
+        //     }, (error) => {
+        //         console.log('getRolesError', error)
+        //     });
+        // }
+        // const getGame = () => {
+        //     db
+        //     .collection("game")
+        //     .doc("operacexxx")
+        //     .onSnapshot(function(doc) {
+        //         setGame(doc.data());
+        //     });
+        // }
         const getBunkers = () => {
             db.collection("bunkers").onSnapshot(function(querySnapshot) {
                 const newBunkers = [];
@@ -67,33 +67,32 @@ const App = () => {
                 setBunkers(newBunkers);
             });
         }
-        getGame();
+        // getGame();
         getBunkers();
-        getRoles();   
-        getCharacters();
+        // getRoles();   
+        // getCharacters();
     }, [])
 
     return (
-      <>
-      <A4>
+    <>
+        <A4>
         <h1>Bunkry</h1>
-      </A4>
+    </A4>
 
-      {bunkers.map(b => {
+    {bunkers.map(b => {
         return (
-          <A4>
-            <h2>{b.name}</h2>
-            <QRCode size={SIZE} value={`bunker:${b.id}`} />
-
-          </A4>
+            <A4>
+                <h2>{b.name}</h2>
+                <QRCode size={SIZE} value={`bunker:${b.id}`} />
+            </A4>
         )
-      })}
+        })}
 
-      <A4>
-        <h2>Tajna svatyne divokych</h2>
-        <QRCode size={SIZE} value="secret-chamber" />
-      </A4>
-      </>
+        <A4>
+            <h2>Tajna svatyne divokych</h2>
+            <QRCode size={SIZE} value="secret-chamber" />
+        </A4>
+    </>
     )
 }
 
