14cc8ff407f52998e340d3e35852da99c167dce2
martin <mvarmuza@gmail.com>
2019-11-17 17:00:07 +0100

    g

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,8 @@ const functions = require('firebase-functions');
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 const intervals = require('./src/interval');
+const { resetGameFn } = require('./src/resetGame');
+
 const { beta, production } = require('./src/config');
 
 const { getRandomInt, getRandomUniqueFromArray } = require('./src/utils');
@@ -118,46 +120,7 @@ exports.surgery = functions.https.onCall(async (data, context) => {
     
 });
 
-exports.resetGameFn = async (data, context) => {
-    console.log('restarting game data');
-    
-    // only meee
-    // if (context.auth.uid !== '3u4h1Fl5zzbiD3Fc2KahEEvKVx82') {
-    //     return 'nemas pravo'
-    // }
 
-    if (!data.type || (data.type !== 'beta' && data.type !== 'production')) {
-        return 'you must specify either "production" or "beta" in params';
-    }
-    const config = data.type === 'production' ? production : beta;
-    
-    console.log('data.type', data.type);
-    console.log('config');
-    
-    console.log(config);
-
-    await db.collection('users').get().then((querySnapshot) => {
-        return querySnapshot.forEach((doc) => {
-            const userRef = db.collection('users').doc(doc.id);
-            return userRef.update({ status: config.initialUserStatus});
-        });
-    }).catch((err) => console.error(err));
-
-    await db.collection('bunkers').get().then((querySnapshot) => {
-        return querySnapshot.forEach((doc) => {
-            const bunkerRef = db.collection('bunkers').doc(doc.id);
-            return bunkerRef.update({
-                'isDestroyed': false,
-                'oxygen': doc.data().oxygenCap,
-            });
-        });
-    }).catch((err) => console.error(err));
-
-    const gameRef = db.collection('game').doc('operacexxx');
-    await gameRef.update(config.initialGame);
-    
-    return 'hra resetovana';
-}
 
 exports.resetGame = functions.https.onCall(async (data, context) => {
     return resetGameFn(data, context);
---
+++ functions/src/resetGame.js
@@ -0,0 +1,45 @@
+const admin = require('firebase-admin');
+const { beta, production } = require('./src/config');
+
+exports.resetGameFn = async (data, context) => {
+    console.log('restarting game data');
+    const db = admin.firestore();
+    
+    // only meee
+    // if (context.auth.uid !== '3u4h1Fl5zzbiD3Fc2KahEEvKVx82') {
+    //     return 'nemas pravo'
+    // }
+
+    if (!data.type || (data.type !== 'beta' && data.type !== 'production')) {
+        return 'you must specify either "production" or "beta" in params';
+    }
+    const config = data.type === 'production' ? production : beta;
+    
+    console.log('data.type', data.type);
+    console.log('config');
+    
+    console.log(config);
+
+    await db.collection('users').get().then((querySnapshot) => {
+        return querySnapshot.forEach((doc) => {
+            const userRef = db.collection('users').doc(doc.id);
+            return userRef.update({ status: config.initialUserStatus});
+        });
+    }).catch((err) => console.error(err));
+
+    await db.collection('bunkers').get().then((querySnapshot) => {
+        return querySnapshot.forEach((doc) => {
+            const bunkerRef = db.collection('bunkers').doc(doc.id);
+            return bunkerRef.update({
+                'isDestroyed': false,
+                'oxygen': doc.data().oxygenCap,
+            });
+        });
+    }).catch((err) => console.error(err));
+
+    const gameRef = db.collection('game').doc('operacexxx');
+    await gameRef.update(config.initialGame);
+    
+    return 'hra resetovana';
+}
+
